4bbdfff4bd19b3d47f30ec90c8e56421f58c5c34
pajazapeluda-sketch <pajazapeluda@gmail.com>
2026-02-15 15:47:11 -0800

    Update LivingEntityStoreAttackerMixin.java

--- src/main/java/com/nessie/windriposte/mixin/LivingEntityStoreAttackerMixin.java
+++ src/main/java/com/nessie/windriposte/mixin/LivingEntityStoreAttackerMixin.java
@@ -2,37 +2,21 @@ package com.nessie.windriposte.mixin;
 
 import com.nessie.windriposte.WindRiposteState;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityStoreAttackerMixin implements WindRiposteState {
 
     @Unique private LivingEntity windriposte$lastAttacker;
 
-    @Unique private long windriposte$nextRiposteTick = 0L;
-    @Unique private long windriposte$inhaleTick = 0L;
-
-    @Inject(
-            method = "damage(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/damage/DamageSource;F)Z",
-            at = @At("HEAD")
-    )
-    private void windriposte$storeAttacker(ServerWorld world, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-        if (source == null) return;
-        LivingEntity attacker = (LivingEntity) source.getAttacker();
-        if (attacker != null) {
-            this.windriposte$lastAttacker = attacker;
-        }
-    }
+    @Unique private long windriposte$readyTick = 0L; // 0 = ready immediately on first ever disable
+    @Unique private boolean windriposte$wasShieldCoolingDown = false;
+    @Unique private boolean windriposte$pendingRearm = false;
 
     @Override
     public LivingEntity windriposte$getLastAttacker() {
-        return this.windriposte$lastAttacker;
+        return windriposte$lastAttacker;
     }
 
     @Override
@@ -46,22 +30,32 @@ public abstract class LivingEntityStoreAttackerMixin implements WindRiposteState
     }
 
     @Override
-    public long windriposte$getNextRiposteTick() {
-        return this.windriposte$nextRiposteTick;
+    public long windriposte$getReadyTick() {
+        return windriposte$readyTick;
     }
 
     @Override
-    public void windriposte$setNextRiposteTick(long tick) {
-        this.windriposte$nextRiposteTick = tick;
+    public void windriposte$setReadyTick(long tick) {
+        this.windriposte$readyTick = tick;
     }
 
     @Override
-    public long windriposte$getInhaleTick() {
-        return this.windriposte$inhaleTick;
+    public boolean windriposte$getWasShieldCoolingDown() {
+        return windriposte$wasShieldCoolingDown;
     }
 
     @Override
-    public void windriposte$setInhaleTick(long tick) {
-        this.windriposte$inhaleTick = tick;
+    public void windriposte$setWasShieldCoolingDown(boolean value) {
+        this.windriposte$wasShieldCoolingDown = value;
+    }
+
+    @Override
+    public boolean windriposte$getPendingRearm() {
+        return windriposte$pendingRearm;
+    }
+
+    @Override
+    public void windriposte$setPendingRearm(boolean value) {
+        this.windriposte$pendingRearm = value;
     }
 }
