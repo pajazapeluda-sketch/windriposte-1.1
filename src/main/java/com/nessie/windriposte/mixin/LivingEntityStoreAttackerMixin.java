1778fa4bc2f6703a24f857ff18d2b961820db95d
pajazapeluda-sketch <pajazapeluda@gmail.com>
2026-02-15 15:13:01 -0800

    Update LivingEntityStoreAttackerMixin.java

--- src/main/java/com/nessie/windriposte/mixin/LivingEntityStoreAttackerMixin.java
+++ src/main/java/com/nessie/windriposte/mixin/LivingEntityStoreAttackerMixin.java
@@ -1,11 +1,9 @@
 package com.nessie.windriposte.mixin;
 
 import com.nessie.windriposte.WindRiposteState;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.server.world.ServerWorld;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,9 +15,19 @@ public abstract class LivingEntityStoreAttackerMixin implements WindRiposteState
 
     @Unique private LivingEntity windriposte$lastAttacker;
 
-    @Override
-    public void windriposte$setLastAttacker(LivingEntity attacker) {
-        this.windriposte$lastAttacker = attacker;
+    @Unique private long windriposte$nextRiposteTick = 0L;
+    @Unique private long windriposte$inhaleTick = 0L;
+
+    @Inject(
+            method = "damage(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/damage/DamageSource;F)Z",
+            at = @At("HEAD")
+    )
+    private void windriposte$storeAttacker(ServerWorld world, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        if (source == null) return;
+        LivingEntity attacker = (LivingEntity) source.getAttacker();
+        if (attacker != null) {
+            this.windriposte$lastAttacker = attacker;
+        }
     }
 
     @Override
@@ -28,22 +36,32 @@ public abstract class LivingEntityStoreAttackerMixin implements WindRiposteState
     }
 
     @Override
+    public void windriposte$setLastAttacker(LivingEntity attacker) {
+        this.windriposte$lastAttacker = attacker;
+    }
+
+    @Override
     public void windriposte$clearLastAttacker() {
         this.windriposte$lastAttacker = null;
     }
 
-    @Inject(
-            method = "damage(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/entity/damage/DamageSource;F)Z",
-            at = @At("HEAD")
-    )
-    private void windriposte$captureAttacker(ServerWorld world, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-        LivingEntity self = (LivingEntity)(Object)this;
+    @Override
+    public long windriposte$getNextRiposteTick() {
+        return this.windriposte$nextRiposteTick;
+    }
 
-        @Nullable Entity attackerEntity = source.getAttacker();
-        if (attackerEntity instanceof LivingEntity attacker) {
-            this.windriposte$setLastAttacker(attacker);
-        } else {
-            this.windriposte$clearLastAttacker();
-        }
+    @Override
+    public void windriposte$setNextRiposteTick(long tick) {
+        this.windriposte$nextRiposteTick = tick;
+    }
+
+    @Override
+    public long windriposte$getInhaleTick() {
+        return this.windriposte$inhaleTick;
+    }
+
+    @Override
+    public void windriposte$setInhaleTick(long tick) {
+        this.windriposte$inhaleTick = tick;
     }
 }
