5d42e42a3aba2b5591de895ee4c6d5556f26f528
pajazapeluda-sketch <pajazapeluda@gmail.com>
2026-02-15 17:29:32 -0800

    Update EntityWorldAccessor.java

--- src/main/java/com/nessie/windriposte/mixin/EntityWorldAccessor.java
+++ src/main/java/com/nessie/windriposte/mixin/EntityWorldAccessor.java
@@ -1,10 +1,11 @@
 package com.nessie.windriposte.mixin;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(net.minecraft.entity.Entity.class)
+@Mixin(Entity.class)
 public interface EntityWorldAccessor {
     @Accessor("world")
     World windriposte$getWorld();
