e81aa02e10dd61cc35d1543fa9c985353e570e76
pajazapeluda-sketch <pajazapeluda@gmail.com>
2026-02-15 16:46:50 -0800

    Update LivingEntityFlagMixin.java

--- src/main/java/com/nessie/windriposte/mixin/LivingEntityFlagMixin.java
+++ src/main/java/com/nessie/windriposte/mixin/LivingEntityFlagMixin.java
@@ -1,27 +1,84 @@
 package com.nessie.windriposte.mixin;
 
-import com.nessie.windriposte.WindRiposteFlag;
+import com.nessie.windriposte.WindRiposteState;
 import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(LivingEntity.class)
-public abstract class LivingEntityFlagMixin implements WindRiposteFlag {
+public abstract class LivingEntityFlagMixin implements WindRiposteState {
 
-    @Unique private long windriposte$shieldDisabledTick = -1;
+    @Unique private LivingEntity windriposte$lastAttacker;
+    @Unique private int windriposte$lastLevel;
+
+    @Unique private boolean windriposte$wasShieldCoolingDown = false;
+    @Unique private boolean windriposte$riposteReady = true;
+    @Unique private long windriposte$riposteReadyAtTick = 0L;
+    @Unique private long windriposte$lastShieldDisableTick = -1L;
 
     @Override
-    public void windriposte$markShieldDisabled(long tick) {
-        windriposte$shieldDisabledTick = tick;
+    public LivingEntity windriposte$getLastAttacker() {
+        return windriposte$lastAttacker;
     }
 
     @Override
-    public long windriposte$getShieldDisabledTick() {
-        return windriposte$shieldDisabledTick;
+    public void windriposte$setLastAttacker(LivingEntity attacker) {
+        windriposte$lastAttacker = attacker;
     }
 
     @Override
-    public void windriposte$clearShieldDisabled() {
-        windriposte$shieldDisabledTick = -1;
+    public int windriposte$getLastLevel() {
+        return windriposte$lastLevel;
+    }
+
+    @Override
+    public void windriposte$setLastLevel(int level) {
+        windriposte$lastLevel = level;
+    }
+
+    @Override
+    public void windriposte$clearLastAttacker() {
+        windriposte$lastAttacker = null;
+        windriposte$lastLevel = 0;
+    }
+
+    @Override
+    public boolean windriposte$getWasShieldCoolingDown() {
+        return windriposte$wasShieldCoolingDown;
+    }
+
+    @Override
+    public void windriposte$setWasShieldCoolingDown(boolean v) {
+        windriposte$wasShieldCoolingDown = v;
+    }
+
+    @Override
+    public boolean windriposte$getRiposteReady() {
+        return windriposte$riposteReady;
+    }
+
+    @Override
+    public void windriposte$setRiposteReady(boolean v) {
+        windriposte$riposteReady = v;
+    }
+
+    @Override
+    public long windriposte$getRiposteReadyAtTick() {
+        return windriposte$riposteReadyAtTick;
+    }
+
+    @Override
+    public void windriposte$setRiposteReadyAtTick(long tick) {
+        windriposte$riposteReadyAtTick = tick;
+    }
+
+    @Override
+    public long windriposte$getLastShieldDisableTick() {
+        return windriposte$lastShieldDisableTick;
+    }
+
+    @Override
+    public void windriposte$setLastShieldDisableTick(long tick) {
+        windriposte$lastShieldDisableTick = tick;
     }
 }
